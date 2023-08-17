706db390efe169638b8452b47973aba50d2684f5
Pavel Vondra <pavelvondra66@gmail.com>
2023-08-17 12:16:45 +0200

    Fix more stories and update snapshot

--- src/components/organisms/articleFeed/articleFeed.stories.tsx
+++ src/components/organisms/articleFeed/articleFeed.stories.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import ArticleFeed from './ArticleFeed';
 
-import articlesJSON from '../../../__mocks__/asyncData/get/allArticlesResponse.mock.json';
-
-export const ArticleFeedLoading = () => (
-  <ArticleFeed isLoading="loading" items={[]} />
-);
-export const ArticleFeedEmpty = () => (
-  <ArticleFeed isLoading={'idle'} items={[]} />
-);
-export const ArticleFeedFull = () => (
-  <ArticleFeed isLoading="idle" items={articlesJSON.data} />
-);
+export const ArticleFeedLoading = () => <ArticleFeed />;
 
 export default {
   title: 'Organisms/Article Feed',
