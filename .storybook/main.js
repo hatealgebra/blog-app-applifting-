30474065e4f33487eec553a88d2f92d4b54db47a
Pavel Vondra <pavelvondra66@gmail.com>
2023-08-18 08:27:36 +0200

    Updat storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
 staticDirs : ['../static'],
   webpackFinal: async (config) => {
     // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-    config.module.rules[2].exclude = [
+    config.module.rules[0].exclude = [
       /node_modules\/(?!(gatsby|gatsby-script)\/)/,
     ];
 
@@ -30,8 +30,13 @@ staticDirs : ['../static'],
     // Remove core-js to prevent issues with Storybook
     config.module.rules[0].exclude = [/core-js/];
     // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
-    config.module.rules[0].use[0].options.plugins.push(
-      require.resolve('babel-plugin-remove-graphql-queries')
+    config.module.rules[0].use[0].options.plugins.push([
+      require.resolve('babel-plugin-remove-graphql-queries'),
+      {
+        stage: config.mode === `development` ? 'develop-html' : 'build-html',
+        staticQueryDir: './page-data/sq/d'
+      }
+    ]
     );
 
     config.resolve.mainFields = ['browser', 'module', 'main'];
