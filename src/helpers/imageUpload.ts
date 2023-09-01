a4b217843435bc795b0ff4e7679e8f3c3bf204db
Pavel Vondra <pavelvondra66@gmail.com>
2023-09-01 16:25:04 +0200

    Add new env for AWS

--- src/helpers/imageUpload.ts
+++ src/helpers/imageUpload.ts
@@ -1,21 +1,22 @@
 const { S3 } = require('@aws-sdk/client-s3');
 const { Upload } = require('@aws-sdk/lib-storage');
 
-const UPLOAD_BUCKET = 'blogappimagesnapshots';
+const { S3_BUCKET_NAME, ASW_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } =
+  process.env;
 
 const uploadImage = async (imageData, key) => {
   const s3 = new S3({
     region: 'eu-north-1',
     credentials: {
-      accessKeyId: '',
-      secretAccessKey: '',
+      accessKeyId: ASW_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
     },
   });
 
   const mediaUpload = new Upload({
     client: s3,
     params: {
-      Bucket: UPLOAD_BUCKET,
+      Bucket: S3_BUCKET_NAME,
       Key: key,
       Body: imageData,
       ACL: 'public-read',
