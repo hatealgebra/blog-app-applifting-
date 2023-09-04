26d10ebcee84b700635ad4fcb18cdf7ad3ad01f0
Pavel Vondra <pavelvondra66@gmail.com>
2023-09-04 21:23:10 +0200

    Restructure the imageUpload file. Add upload diff function

--- src/components/organisms/readArticle/readArticle.stories.tsx
+++ src/components/organisms/readArticle/readArticle.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
+import articlesDetailResponseMockJSON from '@mocks/asyncData/get/articlesDetailsResponse.mock.json';
 import ReadArticle from './ReadArticle';
-import britishCat from '../../../images/british-haircat.jpg';
 
 const content = `Men might grow a beard or maintain a mustache just for extra style points, but the facial hair of the cat has nothing to do with fashion. A cat's whiskers — or vibrissae — are a well-honed sensory tool that helps a catsee in the dark and steer clear of hungry predators. Whiskers
 `;
@@ -9,7 +9,7 @@ const content = `Men might grow a beard or maintain a mustache just for extra st
 export const ReadArticleExample = () => (
   <ReadArticle
     title="Why do cats have Whiskers?"
-    imageSrc={britishCat}
+    imageSrc={`data:image/png;base64,${articlesDetailResponseMockJSON.items[0].imageBase64}`}
     author="Elizabeth whatever"
     createdAt="12.04.2019"
     content={content}
--- src/helpers/imageUpload.ts
+++ src/helpers/imageUpload.ts
@@ -1,23 +1,24 @@
 const { S3 } = require('@aws-sdk/client-s3');
 const { Upload } = require('@aws-sdk/lib-storage');
+const fs = require('fs');
 
-const { S3_BUCKET_NAME, ASW_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } =
+const { S3_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } =
   process.env;
 
-const uploadImage = async (imageData, key) => {
-  const s3 = new S3({
-    region: 'eu-north-1',
-    credentials: {
-      accessKeyId: ASW_ACCESS_KEY_ID,
-      secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    },
-  });
+const s3 = new S3({
+  region: 'eu-north-1',
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  },
+});
 
+export const imageUpload = async (imageData, key) => {
   const mediaUpload = new Upload({
     client: s3,
     params: {
       Bucket: S3_BUCKET_NAME,
-      Key: key,
+      Key: `snapshots/${key}.png`,
       Body: imageData,
       ACL: 'public-read',
     },
@@ -26,4 +27,18 @@ const uploadImage = async (imageData, key) => {
   mediaUpload.done();
 };
 
-export default uploadImage;
+export const uploadDiff = async (diffPath: string, key) => {
+  const imageData = fs.readFileSync(diffPath);
+
+  const mediaUpload = new Upload({
+    client: s3,
+    params: {
+      Bucket: S3_BUCKET_NAME,
+      Key: `diffs/${key}-diff.png`,
+      Body: imageData,
+      ACL: 'public-read',
+    },
+  });
+
+  mediaUpload.done();
+};
